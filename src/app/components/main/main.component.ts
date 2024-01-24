c9d46ee7e18fdae82570a0b994825fb128083434
Elias Fernandes <eliasfernandescout@gmail.com>
2024-01-24 19:04:49 -0300

    update

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import {HeaderComponent} from "../header/header.component";
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, NgOptimizedImage],
   providers: [],
 
   templateUrl: './main.component.html',
